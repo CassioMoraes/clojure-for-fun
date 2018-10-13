d32c317378c6ced5ec6f898c8d146d7e933f965e
Cassio Moraes <cassio.castilhos@gmail.com>
2018-10-13 19:22:27 -0300

    Running clojure with Docker

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,7 @@
 (ns clojure-noob.core
   (:gen-class))
 
-(defn -main
+(defn  -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot!"))
+  (println "I'm a beautiful little teapot!"))
