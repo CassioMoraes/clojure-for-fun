dd57d85a3d3681947849f2ec04fed8c9b5c2a5f3
Cassio Moraes <cassio.castilhos@gmail.com>
2018-10-13 20:16:23 -0300

    Clojure basics

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,4 +4,39 @@
 (defn  -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a beautiful little teapot!"))
+  (println "I'm a beautiful little teapot!")
+
+  (println 1)
+  (println "a string")
+  (println ["a" "vector" "of" "strings"])
+
+  (println (if true
+             "By Zeus's hammer!"
+             "By Aquaman's trident!"))
+
+  (println (if false
+             "By Zeus's hammer!"
+             "By Aquaman's trident!"))
+
+  (println (if false
+             "By Odin's Elbow!"))
+
+  (println (nil? 1))
+
+  (println (nil? nil))
+  
+  (println (= 1 1))
+  
+  (println (= nil nil))
+    
+  (println (= 1 2))
+  
+  (println (or false nil :large_I_mean_venti :why_cant_I_just_say_large))
+  
+  (println (or (= 0 1) (= "yes" "no")))
+
+  (println (or nil))
+
+  (println (and :free_wifi :hot_coffee))
+  
+  (println (and :feelin_super_cool nil false)))
